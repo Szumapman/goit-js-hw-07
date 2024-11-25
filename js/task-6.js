b66670f87680cad70e4b3b09bf4fdef16c27d8ca
Paweł Szumański <pawel.szumanski@outlook.com>
2024-11-25 19:12:11 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector("#boxes");
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const amountInput = document.querySelector("input");
+
+createButton.addEventListener("click", () => {
+  const amount = amountInput.value;
+  if (amount > 0 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    amountInput.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
