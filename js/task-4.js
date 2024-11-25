ccfd7d88792bb5f6edfcd52eb9fd365878629498
Paweł Szumański <pawel.szumanski@outlook.com>
2024-11-25 18:37:47 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value.trim();
+  const password = formElements.password.value.trim();
+  if (email === "" || password === "") {
+    alert('All form fields must be filled in');
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
