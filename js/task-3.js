7c58715ecae84bae7447b67fcb5af0bc86886baa
Paweł Szumański <pawel.szumanski@outlook.com>
2024-11-25 18:23:01 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,10 +3,7 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
-// if you want to see the output name change when the input field loses focus,
-nameInput.addEventListener("change", (event) => {
-// or as below, if you want to see the output name change after typing each letter
-// nameInput.addEventListener("input", (event) => {
+nameInput.addEventListener("input", (event) => {
     const name = event.currentTarget.value.trim();
     nameOutput.textContent = name === "" ? "Anonymous" : name;
 });
